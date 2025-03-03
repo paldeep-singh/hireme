13c69cedec998867f8a2e0d2875b9cb7395396bc
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-03 17:49:08 +1100

    Add test integration test

--- api/src/model/__tests__/companies.integration.test.ts
+++ api/src/model/__tests__/companies.integration.test.ts
@@ -1,3 +1,11 @@
-it("new test", () => {
-  expect(1).toBe(1);
+import { createCompany, getCompanyByName } from "../companies";
+
+it("createCompany", async () => {
+  const result = await createCompany("test3");
+  console.log(result);
+
+  const company = await getCompanyByName("test3");
+
+  expect(company.name).toBe("test3");
+  console.log(company);
 });
--- api/src/model/companies.ts
+++ api/src/model/companies.ts
@@ -9,3 +9,29 @@ export const getAllCompanies = async () => {
     throw new Error(`Database query failed: ${error}`);
   }
 };
+
+export const createCompany = async (name: string) => {
+  try {
+    const result = await db.none(
+      "INSERT INTO companies (id, name) VALUES (gen_random_uuid(), $1)",
+      [name]
+    );
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Database query failed: ${error}`);
+  }
+};
+
+export const getCompanyByName = async (name: string) => {
+  try {
+    const company = await db.one("SELECT * FROM companies WHERE name = $1", [
+      name,
+    ]);
+    return company;
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Database query failed: ${error}`);
+  }
+};
