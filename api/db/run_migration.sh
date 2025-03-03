1c61dc177070c383e6297b2805cef37d8fba0fa1
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-03 17:48:03 +1100

    Fix docker issues

--- api/db/run_migration.sh
+++ api/db/run_migration.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Wait for PostgreSQL to be ready
-until pg_isready -h db -p 5432 -U myuser; do
+until pg_isready -h db -p 5432 -U test_user; do
   echo "Waiting for PostgreSQL..."
   sleep 2
 done
