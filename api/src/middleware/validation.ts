f5658140130b11694f6d26c261f2c0fd9c5d5969
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-05 17:43:31 +1100

    Add type to body validator function

--- api/src/middleware/validation.ts
+++ api/src/middleware/validation.ts
@@ -1,8 +1,10 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, RequestHandler } from "express";
 import { z, ZodError } from "zod";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
 
-export function validateRequestBody<Schema extends z.Schema>(schema: Schema) {
+export function validateRequestBody<Schema extends z.Schema>(
+  schema: Schema
+): RequestHandler {
   // never is used in Request params as we do not need to know the types of the request parameters
   // or the response body at the moment.
   return (req: Request, res: Response, next: NextFunction) => {
