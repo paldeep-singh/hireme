f82154fdc37bf148ec524bd1025024766427ffb8
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-05 16:43:11 +1100

    Add create company handler

---
+++ api/src/controllers/company.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import { CompanyInitializer } from "../../generatedTypes/hire_me/Company";
+import { companyModel } from "../models/company";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+
+export function handleCreateCompany(
+  req: Request<ParamsDictionary, CompanyInitializer>,
+  res: Response
+) {
+  const { name } = req.body;
+
+  try {
+    const company = companyModel.createCompany(name);
+    res.status(StatusCodes.CREATED).json(company);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: error.message,
+      });
+      return;
+    }
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: ReasonPhrases.INTERNAL_SERVER_ERROR,
+    });
+  }
+}
