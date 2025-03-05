e494760b0db57089b8aa7f9f5f5971cf8091ad9b
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-05 16:43:36 +1100

    Include company router routes in root router

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import db from "./models/db";
 import bodyParser from "body-parser";
+import { companyRouter } from "./routes/company";
 
 dotenv.config();
 
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (_: Request, res: Response) => {
   try {
-    const users = await db.any("SELECT * FROM companies");
+    const users = await db.any("SELECT * FROM company");
     res.json(users);
   } catch (error) {
     console.error(error);
@@ -20,19 +21,21 @@ app.get("/", async (_: Request, res: Response) => {
   }
 });
 
-app.post("/company", async (req: Request, res: Response) => {
-  try {
-    const { name } = req.body;
-    await db.none(
-      "INSERT INTO hire_me.companies (id, name) VALUES (gen_random_uuid(), $1)",
-      [name]
-    );
-    res.status(201).json({ message: "Company created" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Database query failed" });
-  }
-});
+app.use("/api", companyRouter);
+
+// app.post("/company", async (req: Request, res: Response) => {
+//   try {
+//     const { name } = req.body;
+//     await db.none(
+//       "INSERT INTO hire_me.companies (id, name) VALUES (gen_random_uuid(), $1)",
+//       [name]
+//     );
+//     res.status(201).json({ message: "Company created" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Database query failed" });
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
