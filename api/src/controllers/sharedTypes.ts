5d22eb2e1b5ebd1bf716d08e195eb9c6ba9a72b8
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-07 17:06:02 +1100

    Change default request body type

--- api/src/controllers/sharedTypes.ts
+++ api/src/controllers/sharedTypes.ts
@@ -3,7 +3,10 @@ import { ParamsDictionary } from "express-serve-static-core";
 
 type ErrorResponseBody = { error: string };
 
-export type RequestHandler<ResBody, ReqBody = object> = ExpressRequestHandler<
+export type RequestHandler<
+  ResBody,
+  ReqBody = undefined,
+> = ExpressRequestHandler<
   ParamsDictionary,
   ResBody | ErrorResponseBody,
   ReqBody
