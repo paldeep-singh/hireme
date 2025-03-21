545a498f00acd3d2d2193a9d505c284c8c01f291
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-21 14:50:22 +0800

    Add authoriser to role routes

--- api/src/routes/__tests__/role.integration.test.ts
+++ api/src/routes/__tests__/role.integration.test.ts
@@ -3,14 +3,20 @@ import api from "../../api.js";
 import { validationErrorCodes } from "../../middleware/validation.js";
 import Company from "shared/generated/db/hire_me/Company.js";
 import {
+  clearAdminTable,
   clearCompanyTable,
   clearRoleTable,
+  clearSessionTable,
+  seedAdmin,
+  seedAdminSession,
   seedCompanies,
   seedRole,
 } from "../../testUtils/dbHelpers.js";
 import { generateRoleData } from "../../testUtils/index.js";
 import db from "../../models/db.js";
 import { RolePreview } from "../../models/role.js";
+import Session from "shared/generated/db/hire_me/session.js";
+import { authorisationrErrors } from "../../middleware/authorisation.js";
 
 afterAll(async () => {
   await db.$pool.end(); // Close the pool after each test file
@@ -28,37 +34,79 @@ describe("POST /api/role", () => {
     await clearCompanyTable();
   });
 
-  describe("when valid body is provided", () => {
-    it("returns statusCode 201", async () => {
-      const requirementData = generateRoleData(company.id);
+  describe("when a valid session is provided", () => {
+    let session: Session;
 
-      const response = await request(api)
-        .post("/api/role")
-        .send(requirementData);
-      expect(response.status).toBe(201);
+    beforeEach(async () => {
+      const admin = await seedAdmin();
+      session = await seedAdminSession(admin.id);
     });
 
-    it("returns the role", async () => {
-      const requirementData = generateRoleData(company.id);
+    afterEach(async () => {
+      await clearSessionTable();
+      await clearAdminTable();
+    });
 
-      const {
-        body: { id, ...rest },
-      } = await request(api).post("/api/role").send(requirementData);
+    describe("when valid body is provided", () => {
+      it("returns statusCode 201", async () => {
+        const roleData = generateRoleData(company.id);
 
-      expect(id).toBeNumber();
-      expect(rest).toEqual(requirementData);
+        const response = await request(api)
+          .post("/api/role")
+          .set("Cookie", [`session=${session.id}`])
+          .send(roleData);
+        expect(response.status).toBe(201);
+      });
+
+      it("returns the role", async () => {
+        const roleData = generateRoleData(company.id);
+
+        const {
+          body: { id, ...rest },
+        } = await request(api)
+          .post("/api/role")
+          .set("Cookie", [`session=${session.id}`])
+          .send(roleData);
+
+        expect(id).toBeNumber();
+        expect(rest).toEqual(roleData);
+      });
+    });
+
+    describe("when invalid body is provided", () => {
+      it("returns statusCode 400", async () => {
+        const response = await request(api)
+          .post("/api/role")
+          .set("Cookie", [`session=${session.id}`])
+          .send({});
+        expect(response.status).toBe(400);
+      });
+
+      it("returns an INVALID_DATA error message", async () => {
+        const response = await request(api)
+          .post("/api/role")
+          .set("Cookie", [`session=${session.id}`])
+          .send({});
+        expect(response.body.error).toEqual(validationErrorCodes.INVALID_DATA);
+      });
     });
   });
 
-  describe("when invalid body is provided", () => {
+  describe("when no session is provided", () => {
     it("returns statusCode 400", async () => {
-      const response = await request(api).post("/api/role").send({});
+      const roleData = generateRoleData(company.id);
+
+      const response = await request(api).post("/api/role").send(roleData);
+
       expect(response.status).toBe(400);
     });
 
-    it("returns an INVALID_DATA error message", async () => {
-      const response = await request(api).post("/api/role").send({});
-      expect(response.body.error).toEqual(validationErrorCodes.INVALID_DATA);
+    it("returns the a BAD_REQUEST error message", async () => {
+      const roleData = generateRoleData(company.id);
+
+      const response = await request(api).post("/api/role").send(roleData);
+
+      expect(response.body.error).toEqual(authorisationrErrors.BAD_REQUEST);
     });
   });
 });
@@ -86,15 +134,47 @@ describe("GET /api/roles/previews", () => {
     await clearCompanyTable();
   });
 
-  it("returns statusCode 200", async () => {
-    const response = await request(api).get("/api/roles/previews");
+  describe("when a valid session is provided", () => {
+    let session: Session;
 
-    expect(response.status).toBe(200);
+    beforeEach(async () => {
+      const admin = await seedAdmin();
+      session = await seedAdminSession(admin.id);
+    });
+
+    afterEach(async () => {
+      await clearSessionTable();
+      await clearAdminTable();
+    });
+
+    it("returns statusCode 200", async () => {
+      const response = await request(api)
+        .get("/api/roles/previews")
+        .set("Cookie", [`session=${session.id}`]);
+
+      expect(response.status).toBe(200);
+    });
+
+    it("returns an array of application previews", async () => {
+      const response = await request(api)
+        .get("/api/roles/previews")
+        .set("Cookie", [`session=${session.id}`]);
+
+      expect(response.body).toIncludeSameMembers(rolePreviews);
+    });
   });
 
-  it("returns an array of application previews", async () => {
-    const response = await request(api).get("/api/roles/previews");
+  describe("when no session is provided", () => {
+    it("returns statusCode 400", async () => {
+      const response = await request(api).get("/api/roles/previews");
 
-    expect(response.body).toIncludeSameMembers(rolePreviews);
+      expect(response.status).toBe(400);
+    });
+
+    it("returns the a BAD_REQUEST error message", async () => {
+      const response = await request(api).get("/api/roles/previews");
+
+      expect(response.body.error).toEqual(authorisationrErrors.BAD_REQUEST);
+    });
   });
 });
--- api/src/routes/role.ts
+++ api/src/routes/role.ts
@@ -2,8 +2,15 @@ import { Router } from "express";
 import { validateRequestBody } from "../middleware/validation.js";
 import { roleInitializer } from "shared/generated/db/hire_me/Role.js";
 import { handleAddRole, handleGetRolePreviews } from "../controllers/role.js";
+import { authoriseRequest } from "../middleware/authorisation.js";
 
 export const roleRouter = Router();
 
-roleRouter.post("/role", validateRequestBody(roleInitializer), handleAddRole);
-roleRouter.get("/roles/previews", handleGetRolePreviews);
+roleRouter.post(
+  "/role",
+  authoriseRequest(),
+  validateRequestBody(roleInitializer),
+  handleAddRole,
+);
+
+roleRouter.get("/roles/previews", authoriseRequest(), handleGetRolePreviews);
