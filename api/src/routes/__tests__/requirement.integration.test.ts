9980e9972baaa4423211cabcbefaa352fb2907bd
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-21 14:41:37 +0800

    Add authoriser to requirement routes

--- api/src/routes/__tests__/requirement.integration.test.ts
+++ api/src/routes/__tests__/requirement.integration.test.ts
@@ -3,8 +3,17 @@ import api from "../../api.js";
 import { validationErrorCodes } from "../../middleware/validation.js";
 import db from "../../models/db.js";
 import { generateRequirementData } from "../../testUtils/index.js";
-import { seedCompanies, seedRole } from "../../testUtils/dbHelpers.js";
+import {
+  clearAdminTable,
+  clearSessionTable,
+  seedAdmin,
+  seedAdminSession,
+  seedCompanies,
+  seedRole,
+} from "../../testUtils/dbHelpers.js";
 import request from "supertest";
+import Session from "shared/generated/db/hire_me/Session.js";
+import { authorisationrErrors } from "../../middleware/authorisation.js";
 
 afterAll(async () => {
   await db.$pool.end(); // Close the pool after each test file
@@ -18,38 +27,84 @@ describe("POST /api/requirement", () => {
     role = await seedRole(company.id);
   });
 
-  describe("when valid body is provided", () => {
-    it("returns status code 201", async () => {
+  describe("when a valid session is provided", () => {
+    let session: Session;
+
+    beforeEach(async () => {
+      const admin = await seedAdmin();
+      session = await seedAdminSession(admin.id);
+    });
+
+    afterEach(async () => {
+      await clearSessionTable();
+      await clearAdminTable();
+    });
+
+    describe("when valid body is provided", () => {
+      it("returns status code 201", async () => {
+        const requirementData = generateRequirementData(role.id);
+
+        const response = await request(api)
+          .post("/api/requirement")
+          .set("Cookie", [`session=${session.id}`])
+          .send(requirementData);
+
+        expect(response.status).toEqual(201);
+      });
+
+      it("returns the requirement", async () => {
+        const requirmentData = generateRequirementData(role.id);
+
+        const {
+          body: { id, ...rest },
+        } = await request(api)
+          .post("/api/requirement")
+          .set("Cookie", [`session=${session.id}`])
+          .send(requirmentData);
+
+        expect(id).toBeNumber();
+        expect(rest).toEqual(requirmentData);
+      });
+    });
+
+    describe("when invalid body is provided", () => {
+      it("returns statusCode 400", async () => {
+        const response = await request(api)
+          .post("/api/requirement")
+          .set("Cookie", [`session=${session.id}`])
+          .send({});
+        expect(response.status).toBe(400);
+      });
+
+      it("returns an INVALID_DATA error message", async () => {
+        const response = await request(api)
+          .post("/api/requirement")
+          .set("Cookie", [`session=${session.id}`])
+          .send({});
+        expect(response.body.error).toEqual(validationErrorCodes.INVALID_DATA);
+      });
+    });
+  });
+
+  describe("when no session is provided", () => {
+    it("returns statusCode 400", async () => {
       const requirementData = generateRequirementData(role.id);
 
       const response = await request(api)
         .post("/api/requirement")
         .send(requirementData);
 
-      expect(response.status).toEqual(201);
-    });
-
-    it("returns the requirement", async () => {
-      const requirmentData = generateRequirementData(role.id);
-
-      const {
-        body: { id, ...rest },
-      } = await request(api).post("/api/requirement").send(requirmentData);
-
-      expect(id).toBeNumber();
-      expect(rest).toEqual(requirmentData);
-    });
-  });
-
-  describe("when invalid body is provided", () => {
-    it("returns statusCode 400", async () => {
-      const response = await request(api).post("/api/requirement").send({});
       expect(response.status).toBe(400);
     });
 
-    it("returns an INVALID_DATA error message", async () => {
-      const response = await request(api).post("/api/requirement").send({});
-      expect(response.body.error).toEqual(validationErrorCodes.INVALID_DATA);
+    it("returns the a BAD_REQUEST error message", async () => {
+      const requirementData = generateRequirementData(role.id);
+
+      const {
+        body: { error },
+      } = await request(api).post("/api/requirement").send(requirementData);
+
+      expect(error).toEqual(authorisationrErrors.BAD_REQUEST);
     });
   });
 });
--- api/src/routes/requirement.ts
+++ api/src/routes/requirement.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { requirementInitializer } from "shared/generated/db/hire_me/Requirement.js";
 import { handleAddRequirement } from "../controllers/requirement.js";
 import { validateRequestBody } from "../middleware/validation.js";
+import { authoriseRequest } from "../middleware/authorisation.js";
 
 export const requirementRouter = Router();
 
 requirementRouter.post(
   "/requirement",
+  authoriseRequest(),
   validateRequestBody(requirementInitializer),
   handleAddRequirement,
 );
