2a23a660f3590884ebd6beb7e30238b1e0a3bcf8
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-26 00:25:33 +0800

    Generate apiFetch types

--- shared/scripts/generateApiFetchTypes.ts
+++ shared/scripts/generateApiFetchTypes.ts
@@ -18,6 +18,13 @@ import {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Delete fetchTypes file to ensure linting of generated route types does not fail.
+try {
+	fs.rmSync(path.resolve(__dirname, "../generated/routes/fetchTypes.ts"));
+} catch {
+	console.log("fetchTypes.ts file not found, proceeding...");
+}
+
 const routeFiles = fs.readdirSync(
 	path.resolve(__dirname, "../../api/src/routes"),
 	{
@@ -51,11 +58,82 @@ routeFiles.forEach((file) => {
 	outputSourceFile.saveSync();
 });
 
-formatWithPrettier("generated/routes/*").then(() =>
+await formatWithPrettier("generated/routes/*").then(() =>
 	console.log("files formatted"),
 );
 
-fixLintingErrors("generated/routes/*").then(() => console.log("files linted."));
+await fixLintingErrors("generated/routes/*").then(() =>
+	console.log("files linted."),
+);
+
+generateFetchTypes();
+
+async function generateFetchTypes() {
+	const routeTypeFiles = fs
+		.readdirSync(path.resolve(__dirname, "../generated/routes"), {
+			withFileTypes: true,
+		})
+		.filter((file) => file.isFile() && file.name !== "fetchTypes.ts");
+
+	fs.writeFileSync(
+		path.resolve(__dirname, "../generated/routes/fetchTypes.ts"),
+		"",
+	);
+
+	const project = new Project();
+
+	const outputPath = path.resolve(
+		__dirname,
+		`../generated/routes/fetchTypes.ts`,
+	);
+
+	const outputSourceFile = project.addSourceFileAtPath(outputPath);
+
+	outputSourceFile.addInterface({
+		name: "ApiRequests",
+		isExported: true,
+		properties: [],
+	});
+
+	outputSourceFile.saveSync();
+
+	const apiFetchInterface = outputSourceFile.getInterfaceOrThrow("ApiRequests");
+
+	routeTypeFiles.forEach((file) => {
+		const sourceFile = project.addSourceFileAtPath(
+			`${file.parentPath}/${file.name}`,
+		);
+
+		sourceFile
+			.getDescendantsOfKind(SyntaxKind.InterfaceDeclaration)
+			.forEach((interfaceDeclaration) => {
+				const name = interfaceDeclaration.getName();
+
+				const action = name.replace("Request", "");
+
+				outputSourceFile.addImportDeclaration({
+					namedImports: [name],
+					moduleSpecifier: `./${file.name.replace(".ts", ".js")}`,
+				});
+
+				console.log(interfaceDeclaration.getText());
+				apiFetchInterface.addProperty({
+					name: action,
+					type: name,
+				});
+			});
+	});
+
+	outputSourceFile.saveSync();
+
+	await formatWithPrettier("generated/routes/fetchTypes.ts").then(() =>
+		console.log("fetchTypes.ts formatted"),
+	);
+
+	await fixLintingErrors("generated/routes/fetchTypes.ts").then(() =>
+		console.log("fetchTypes.ts linted."),
+	);
+}
 
 function parseHandlerTypes(node: Node<ts.Node>, outputSourceFile: SourceFile) {
 	if (!node.isKind(SyntaxKind.CallExpression)) {
