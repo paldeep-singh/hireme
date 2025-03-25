e63180be4523a0a2f253b6d3e0c444c46be6a5be
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-26 00:36:11 +0800

    Add apiFetch util

---
+++ app/src/utils/apiFetch.ts
@@ -0,0 +1,46 @@
+import { ApiRequests } from "shared/generated/routes/fetchTypes.js";
+
+export async function apiFetch<K extends keyof ApiRequests>({
+	path,
+	body,
+	method,
+}: Omit<ApiRequests[K], "response">) {
+	const response = await fetch(path, {
+		method,
+		body: body ? JSON.stringify(body) : undefined,
+		headers: {
+			...(typeof body === "object" && {
+				"Content-Type": "application/json",
+			}),
+		},
+	});
+
+	if (!response.ok) {
+		await parseErrorResponse(response);
+	}
+
+	return await parseSuccessResponse<K>(response);
+}
+
+async function parseSuccessResponse<K extends keyof ApiRequests>(
+	response: Response,
+) {
+	try {
+		const data = (await response.json()) as ApiRequests[K]["responseBody"];
+
+		return data;
+	} catch {
+		throw new Error("Could not parse response data.");
+	}
+}
+
+async function parseErrorResponse(response: Response) {
+	let error: string;
+	try {
+		error = ((await response.json()) as { error: string }).error;
+	} catch {
+		throw new Error("An unknown error occurred, error could not be parsed.");
+	}
+
+	throw new Error(error);
+}
