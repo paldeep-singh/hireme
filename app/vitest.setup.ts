c4673f77794bb7413ffbf50bcc5ae16993443279
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-26 14:58:00 +0800

    Remove fetch mock

--- app/vitest.setup.ts
+++ app/vitest.setup.ts
@@ -1,8 +1,1 @@
 import "@testing-library/jest-dom/vitest";
-import { vi } from "vitest";
-import createFetchMock from "vitest-fetch-mock";
-
-const fetchMocker = createFetchMock(vi);
-
-// sets globalThis.fetch and globalThis.fetchMock to our mocked version
-fetchMocker.enableMocks();
