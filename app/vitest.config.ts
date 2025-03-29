e5c8b2919f85be781d1ba10a2ec04dda6f89ab60
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-29 22:50:15 +0800

    Separate page and unit tests in app

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -5,5 +5,23 @@ export default defineConfig({
 		globals: true,
 		environment: "jsdom",
 		setupFiles: ["./vitest.setup.ts"],
+		exclude: ["node_modules"],
+		workspace: [
+			{
+				extends: true,
+				test: {
+					name: "unit",
+					include: ["**/*.test.ts", "**/*.test.tsx"],
+					exclude: ["**/*.page.test.ts", "**/*.page.test.tsx"],
+				},
+			},
+			{
+				extends: true,
+				test: {
+					name: "page",
+					include: ["**/*.page.test.ts", "**/*.page.test.tsx"],
+				},
+			},
+		],
 	},
 });
