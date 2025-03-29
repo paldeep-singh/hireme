2ce623542d35b3fe383803286a80fdced208ec8a
Paldeep Singh <paldeepsingh21@gmail.com>
2025-03-29 21:32:53 +0800

    Remove jest config

--- api/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-	testEnvironment: "node",
-	transform: {
-		"^.+.tsx?$": ["ts-jest", {}],
-	},
-	setupFiles: ["./jest.setupFiles.ts"],
-	setupFilesAfterEnv: ["jest-extended/all"],
-};
