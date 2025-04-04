753e813131e3ea8a371fdf84529c6df1e5b2ee06
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-04 22:23:26 +0800

    Update role preview type

--- packages/shared/types/rolePreview.ts
+++ packages/shared/types/rolePreview.ts
@@ -1,6 +1,11 @@
+import Application from "../generated/db/hire_me/Application.js";
 import Company from "../generated/db/hire_me/Company.js";
 import Role from "../generated/db/hire_me/Role.js";
+import RoleLocation from "../generated/db/hire_me/RoleLocation.js";
 
-export interface RolePreview extends Role {
+export interface RolePreview
+	extends Role,
+		Pick<RoleLocation, "location">,
+		Pick<Application, "submitted"> {
 	company: Company["name"];
 }
