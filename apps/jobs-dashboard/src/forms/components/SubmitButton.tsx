7f04be1084a87a4ed46299b87d44787920dd9684
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-04 18:50:06 +0800

    Require loading prop for submit button

--- apps/jobs-dashboard/src/forms/components/SubmitButton.tsx
+++ apps/jobs-dashboard/src/forms/components/SubmitButton.tsx
@@ -1,6 +1,6 @@
 import { Button, ButtonProps } from "../../components/Button";
 
-type SubmitButtonProps = Pick<ButtonProps, "label" | "loading">;
+type SubmitButtonProps = Required<Pick<ButtonProps, "label" | "loading">>;
 
 export function SubmitButton({ label, loading }: SubmitButtonProps) {
 	return <Button label={label} loading={loading} type="submit" />;
