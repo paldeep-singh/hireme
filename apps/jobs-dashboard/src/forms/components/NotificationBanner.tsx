fbc6ced70e936d285c6e53f7743caf0bda4f9652
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-04 22:05:50 +0800

    Remove unused styles

--- apps/jobs-dashboard/src/forms/components/NotificationBanner.tsx
+++ apps/jobs-dashboard/src/forms/components/NotificationBanner.tsx
@@ -4,12 +4,7 @@ interface NotificationBannerProps {
 
 export function NotificationBanner({ notification }: NotificationBannerProps) {
 	return notification ? (
-		<div
-			role="alert"
-			aria-live="assertive"
-			aria-atomic="true"
-			className="notification-banner"
-		>
+		<div role="alert" aria-live="assertive" aria-atomic="true">
 			<p>{notification}</p>
 		</div>
 	) : null;
