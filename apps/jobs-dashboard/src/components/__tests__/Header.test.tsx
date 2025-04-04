ccd58d3530bf6489a55b97ca501e10d7283bb59a
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-04 19:08:23 +0800

    Log user out when login button is pressed

--- apps/jobs-dashboard/src/components/__tests__/Header.test.tsx
+++ apps/jobs-dashboard/src/components/__tests__/Header.test.tsx
@@ -1,16 +1,61 @@
-import { render, screen } from "@testing-library/react";
+import { redirect } from "@tanstack/react-router";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { renderWithProviders } from "../../testUtils";
+import { apiFetch } from "../../utils/apiFetch";
 import { Header } from "../Header";
+
+vi.mock("../../utils/apiFetch");
+
+vi.mock(import("@tanstack/react-router"), async (importOriginal) => {
+	const actual = await importOriginal();
+
+	return {
+		...actual,
+		redirect: vi.fn(),
+	};
+});
+
+const mockedApiFetch = vi.mocked(apiFetch);
 
 describe("Header", () => {
 	it("renders the website title", () => {
-		render(<Header />);
+		renderWithProviders(<Header />);
 
 		expect(screen.getByRole("banner")).toHaveTextContent("Jobs Dashboard");
 	});
 
 	it("renders the logout button", () => {
-		render(<Header />);
+		renderWithProviders(<Header />);
 
 		expect(screen.getByRole("button")).toHaveTextContent("Logout");
+	});
+
+	describe("when logout button is clicked", () => {
+		it("makes logout request", async () => {
+			renderWithProviders(<Header />);
+
+			const user = userEvent.setup();
+
+			await user.click(screen.getByRole("button"));
+
+			expect(mockedApiFetch).toHaveBeenCalledWith({
+				path: "/api/admin/logout",
+				method: "delete",
+				body: null,
+			});
+		});
+
+		it("redirects to the login screen", async () => {
+			renderWithProviders(<Header />);
+
+			const user = userEvent.setup();
+
+			await user.click(screen.getByRole("button"));
+
+			expect(redirect).toHaveBeenCalledWith({
+				to: "/login",
+			});
+		});
 	});
 });
