328adf24614d3e2f95fea090b79cfb68b79d4b59
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-05 23:28:23 +0800

    Remove unncessary file extesions

--- packages/shared/types/rolePreview.ts
+++ packages/shared/types/rolePreview.ts
@@ -1,7 +1,7 @@
-import Application from "../generated/db/hire_me/Application.js";
-import Company from "../generated/db/hire_me/Company.js";
-import Role from "../generated/db/hire_me/Role.js";
-import RoleLocation from "../generated/db/hire_me/RoleLocation.js";
+import Application from "../generated/db/hire_me/Application";
+import Company from "../generated/db/hire_me/Company";
+import Role from "../generated/db/hire_me/Role";
+import RoleLocation from "../generated/db/hire_me/RoleLocation";
 
 export interface RolePreview extends Role {
 	company: Company["name"];
