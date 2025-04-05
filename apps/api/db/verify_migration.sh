69c3165db1159e66655ad4265d9b81d5411e592f
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-05 23:03:57 +0800

    Up db in ci without deploying

--- apps/api/db/verify_migration.sh
+++ apps/api/db/verify_migration.sh
@@ -6,4 +6,8 @@ done
 echo "Migration completed, verifying"
 echo "$PWD"
 cd db
-sqitch verify -t hire_me_test_db
+# We use deploy since we we verify on deployment anyway.
+# In CI, migrations won't be automatically applied to the
+# PG docker container since that would make it impossible to 
+# verify if deployment is rolled back due to failed verification.
+sqitch deploy -t hire_me_test_db
