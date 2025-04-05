4161aab75c220c3d118bdffc3f430550d96c9e07
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-06 00:02:34 +0800

    Add roledetails type

--- packages/shared/types/Role.ts
+++ packages/shared/types/Role.ts
@@ -1,5 +1,7 @@
 import Application from "../generated/db/hire_me/Application";
 import Company from "../generated/db/hire_me/Company";
+import Contract from "../generated/db/hire_me/Contract";
+import Requirement from "../generated/db/hire_me/Requirement";
 import Role from "../generated/db/hire_me/Role";
 import RoleLocation from "../generated/db/hire_me/RoleLocation";
 
@@ -13,4 +15,12 @@ export interface RolePreviewJson
 	extends Omit<RolePreview, "date_added" | "date_submitted"> {
 	date_added: string;
 	date_submitted: string | null;
+}
+
+export interface RoleDetails extends Role {
+	company: Company;
+	location: RoleLocation | null;
+	contract: Contract | null;
+	requirements: Requirement[] | null;
+	date_submitted: Application["date_submitted"] | null;
 }
