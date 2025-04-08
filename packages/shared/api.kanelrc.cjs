b3d0ddbc6ca73c7fd7309f7e7d7a055d1b40e6e6
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-08 20:31:14 +0800

    Add pre-render hook to generate more specific zod schemas for certain properties

--- packages/shared/api.kanelrc.cjs
+++ packages/shared/api.kanelrc.cjs
@@ -5,6 +5,7 @@ const {
 	defaultZodTypeMap,
 } = require("kanel-zod");
 
+/** @type {import('kanel-zod').makeGenerateZodSchemas} */
 const generateZodSchemas = makeGenerateZodSchemas({
 	getZodSchemaMetadata: defaultGetZodSchemaMetadata,
 	getZodIdentifierMetadata: defaultGetZodIdentifierMetadata,
@@ -22,6 +23,74 @@ const generateZodSchemas = makeGenerateZodSchemas({
 	},
 });
 
+/**
+ * @typedef {Object} SchemaConfig
+ * @property {string} attribute - The attribute to match.
+ * @property {string} search - The string to search for.
+ * @property {string} replacement - The string to replace matches with.
+ */
+
+/**@type {SchemaConfig[]} */
+const specificZodConfigs = [
+	{
+		attribute: "email",
+		search: "z.string()",
+		replacement: "z.string().email()",
+	},
+];
+
+/**
+ * @param {import("kanel").Output} output
+ * @param {SchemaConfig[]} configs - An array of schema configuration objects.
+ * @returns {import("kanel").Output}
+ */
+
+function specificZodSchemas(output, configs) {
+	const updatedOutput = Object.entries(output).map(
+		([path, { declarations }]) => {
+			const updatedDeclarations = declarations.map((dec) => {
+				if (!(dec.declarationType === "constant")) {
+					return dec;
+				}
+
+				const { value } = dec;
+
+				if (typeof value === "string") {
+					return dec;
+				}
+
+				if (!value[0].startsWith("z.object")) {
+					return dec;
+				}
+
+				configs.forEach(({ attribute, replacement, search }) => {
+					const attributeIndex = value.findIndex((v) => {
+						return v.trimStart().startsWith(attribute);
+					});
+
+					if (attributeIndex === -1) {
+						return;
+					}
+
+					value[attributeIndex] = value[attributeIndex].replace(
+						search,
+						replacement,
+					);
+				});
+
+				return {
+					...dec,
+					value,
+				};
+			});
+
+			return [path, { declarations: updatedDeclarations }];
+		},
+	);
+
+	return Object.fromEntries(updatedOutput);
+}
+
 /** @type {import('kanel').Config} */
 module.exports = {
 	connection: {
@@ -35,7 +104,10 @@ module.exports = {
 	enumStyle: "type",
 	preDeleteOutputFolder: true,
 	outputPath: "./generated/api",
-	preRenderHooks: [generateZodSchemas],
+	preRenderHooks: [
+		generateZodSchemas,
+		(output) => specificZodSchemas(output, specificZodConfigs),
+	],
 	customTypeMap: {
 		"pg_catalog.numrange": {
 			name: "NumRange",
