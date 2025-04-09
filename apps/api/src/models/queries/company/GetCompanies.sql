746da224a1d93721ae02a7515d91b0f2e39f6c03
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-10 01:16:24 +0800

    Add sql query files

---
+++ apps/api/src/models/queries/company/GetCompanies.sql
@@ -0,0 +1,11 @@
+/* @name GetCompanies */
+SELECT
+	id,
+	name,
+	notes,
+	website
+FROM
+	hire_me.company
+ORDER BY
+	name;
+
---
+++ apps/api/src/models/queries/requirement/AddRequirement.sql
@@ -0,0 +1,6 @@
+/* @name AddRequirement */
+INSERT INTO hire_me.requirement (role_id, bonus, description)
+	VALUES (:role_id, :bonus, :description)
+RETURNING
+	id, role_id, bonus, description;
+
