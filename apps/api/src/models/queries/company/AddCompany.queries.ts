6c9a1ce3e39d4a19ccdb21a2836318e55170eb13
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-11 15:31:18 +0800

    Use pgtyped queries in company model

--- apps/api/src/models/queries/company/AddCompany.queries.ts
+++ apps/api/src/models/queries/company/AddCompany.queries.ts
@@ -4,8 +4,8 @@ import { PreparedQuery } from '@pgtyped/runtime';
 /** 'AddCompany' parameters type */
 export interface IAddCompanyParams {
   name: string;
-  notes: string;
-  website: string;
+  notes?: string | null | void;
+  website?: string | null | void;
 }
 
 /** 'AddCompany' return type */
@@ -22,13 +22,13 @@ export interface IAddCompanyQuery {
   result: IAddCompanyResult;
 }
 
-const addCompanyIR: any = {"usedParamSet":{"name":true,"notes":true,"website":true},"params":[{"name":"name","required":true,"transform":{"type":"scalar"},"locs":[{"a":60,"b":66}]},{"name":"notes","required":true,"transform":{"type":"scalar"},"locs":[{"a":69,"b":76}]},{"name":"website","required":true,"transform":{"type":"scalar"},"locs":[{"a":79,"b":88}]}],"statement":"INSERT INTO hire_me.company (name, notes, website)\n\tVALUES (:name !, :notes !, :website !)\nRETURNING\n\tid, name, notes, website"};
+const addCompanyIR: any = {"usedParamSet":{"name":true,"notes":true,"website":true},"params":[{"name":"name","required":true,"transform":{"type":"scalar"},"locs":[{"a":60,"b":66}]},{"name":"notes","required":false,"transform":{"type":"scalar"},"locs":[{"a":69,"b":74}]},{"name":"website","required":false,"transform":{"type":"scalar"},"locs":[{"a":77,"b":84}]}],"statement":"INSERT INTO hire_me.company (name, notes, website)\n\tVALUES (:name !, :notes, :website)\nRETURNING\n\tid, name, notes, website"};
 
 /**
  * Query generated from SQL:
  * ```
  * INSERT INTO hire_me.company (name, notes, website)
- * 	VALUES (:name !, :notes !, :website !)
+ * 	VALUES (:name !, :notes, :website)
  * RETURNING
  * 	id, name, notes, website
  * ```
