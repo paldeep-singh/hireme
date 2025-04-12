f3ec526941d643acd1adda83f9b6768e3219a1b8
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-12 19:13:38 +0800

    Use kysely in db helpers

--- apps/api/src/testUtils/dbHelpers.ts
+++ apps/api/src/testUtils/dbHelpers.ts
@@ -1,6 +1,5 @@
 import DBAdmin, { AdminId } from "@repo/shared/generated/db/hire_me/Admin";
 import DBApplication from "@repo/shared/generated/db/hire_me/Application";
-import DBCompany from "@repo/shared/generated/db/hire_me/Company";
 import DBRequirement from "@repo/shared/generated/db/hire_me/Requirement";
 import DBRole, { RoleId } from "@repo/shared/generated/db/hire_me/Role";
 import DBRoleLocation from "@repo/shared/generated/db/hire_me/RoleLocation";
@@ -14,46 +13,68 @@ import {
 	generateRoleData,
 	generateRoleLocationData,
 } from "@repo/shared/testHelpers/generators";
-import testDb from "./testDb";
+import { sql } from "kysely";
+import { db } from "../db/database";
+import { CompanyId } from "../db/generated/hire_me/Company";
 
-export async function seedCompanies(count: number): Promise<DBCompany[]> {
+export async function seedCompanies(count: number) {
 	const companydata = Array.from({ length: count }, () =>
 		generateCompanyData(),
 	);
 	const companies = await Promise.all(
 		companydata.map(({ name, notes, website }) =>
-			testDb.one(
-				"INSERT INTO company (name, notes, website) VALUES ($1, $2, $3) RETURNING id, name, notes, website",
-				[name, notes, website],
-			),
+			db
+				.withSchema("hire_me")
+				.insertInto("company")
+				.values([
+					{
+						name,
+						notes,
+						website,
+					},
+				])
+				.returningAll()
+				.executeTakeFirstOrThrow(),
 		),
 	);
 	return companies;
 }
 
 export async function clearCompanyTable(): Promise<void> {
-	await testDb.none("TRUNCATE TABLE company RESTART IDENTITY CASCADE");
+	await sql<void>`TRUNCATE TABLE hire_me.company RESTART IDENTITY CASCADE`.execute(
+		db,
+	);
 }
 
 export async function clearRoleTable(): Promise<void> {
-	await testDb.none("TRUNCATE TABLE role RESTART IDENTITY CASCADE");
+	await sql<void>`TRUNCATE TABLE hire_me.role RESTART IDENTITY CASCADE`.execute(
+		db,
+	);
 }
 
 export async function clearAdminTable(): Promise<void> {
-	await testDb.none("TRUNCATE TABLE admin RESTART IDENTITY CASCADE");
+	await sql<void>`TRUNCATE TABLE hire_me.admin RESTART IDENTITY CASCADE`.execute(
+		db,
+	);
 }
 
 export async function clearSessionTable(): Promise<void> {
-	await testDb.none("TRUNCATE TABLE session");
+	await sql<void>`TRUNCATE TABLE hire_me.session`.execute(db);
 }
 
-export async function seedRole(companyId: number): Promise<DBRole> {
+export async function seedRole(companyId: CompanyId): Promise<DBRole> {
 	const { title, ad_url, notes } = generateRoleData(companyId);
-	const role = await testDb.one<DBRole>(
-		`INSERT INTO role (company_id, title, notes, ad_url) VALUES ($1, $2, $3, $4) 
-      RETURNING id, company_id, title, notes, ad_url, date_added`,
-		[companyId, title, notes ?? null, ad_url ?? null],
-	);
+	const role = await db
+		.withSchema("hire_me")
+		.insertInto("role")
+		.values({
+			title,
+			ad_url,
+			notes,
+			company_id: companyId,
+		})
+		.returningAll()
+		.executeTakeFirstOrThrow();
 
 	return role;
 }
@@ -64,13 +85,12 @@ export async function seedRoleLocation(
 	const { hybrid, location, office_days, on_site, remote, role_id } =
 		generateRoleLocationData(roleId);
 
-	const roleLocation = await testDb.one<DBRoleLocation>(
-		`INSERT INTO role_location (role_id, location, hybrid, remote, on_site, office_days)
-		 VALUES ($1, $2, $3, $4, $5, $6)
-		 RETURNING id, role_id, location, hybrid, remote, on_site, office_days
-		`,
-		[role_id, location, hybrid, remote, on_site, office_days],
-	);
+	const roleLocation = await db
+		.withSchema("hire_me")
+		.insertInto("role_location")
+		.values({ hybrid, location, office_days, on_site, remote, role_id })
+		.returningAll()
+		.executeTakeFirstOrThrow();
 
 	return roleLocation;
 }
@@ -79,12 +99,12 @@ export async function seedApplication(roleId: RoleId): Promise<DBApplication> {
 	const { cover_letter, date_submitted, role_id } =
 		generateApplicationData(roleId);
 
-	const application = await testDb.one<DBApplication>(
-		`INSERT INTO application (cover_letter, date_submitted, role_id)
-		VALUES ($1, $2, $3)
-		RETURNING id, cover_letter, date_submitted, role_id`,
-		[cover_letter, date_submitted, role_id],
-	);
+	const application = db
+		.withSchema("hire_me")
+		.insertInto("application")
+		.values({ cover_letter, date_submitted, role_id })
+		.returningAll()
+		.executeTakeFirstOrThrow();
 
 	return application;
 }
@@ -92,12 +112,13 @@ export async function seedApplication(roleId: RoleId): Promise<DBApplication> {
 export async function seedRequirement(roleId: number): Promise<DBRequirement> {
 	const { bonus, description, role_id } = generateRequirementData(roleId);
 
-	const requirement = await testDb.one<DBRequirement>(
-		`INSERT INTO requirement (role_id, bonus, description)
-            VALUES ($1, $2, $3)
-            RETURNING id, role_id, bonus, description`,
-		[role_id, bonus, description],
-	);
+	const requirement = db
+		.withSchema("hire_me")
+		.insertInto("requirement")
+		.values({ bonus, description, role_id })
+		.returningAll()
+		.executeTakeFirstOrThrow();
+
 	return requirement;
 }
 
@@ -106,13 +127,12 @@ export async function seedAdmin(
 ): Promise<DBAdmin & { password: string }> {
 	const { email, password_hash, password } = await generateAdminData();
 
-	const adminDetails = await testDb.one<DBAdmin>(
-		`
-    INSERT INTO admin (email, password_hash) 
-    VALUES ($1, $2) 
-    RETURNING id, email, password_hash`,
-		[emailOverride ?? email, password_hash],
-	);
+	const adminDetails = await db
+		.withSchema("hire_me")
+		.insertInto("admin")
+		.values({ email: emailOverride ?? email, password_hash })
+		.returningAll()
+		.executeTakeFirstOrThrow();
 
 	return {
 		...adminDetails,
@@ -126,14 +146,12 @@ export async function seedAdminSession(
 ): Promise<DBSession> {
 	const { expiry, id } = generateAdminSession(adminId);
 
-	const sessionDetails = await testDb.one<DBSession>(
-		`
-    INSERT INTO session (id, expiry, admin_id)
-    VALUES ($1, $2, $3)
-    RETURNING id, expiry, admin_id
-    `,
-		[id, overrideExpiry ?? expiry, adminId],
-	);
+	const sessionDetails = db
+		.withSchema("hire_me")
+		.insertInto("session")
+		.values({ expiry: overrideExpiry ?? expiry, id, admin_id: adminId })
+		.returningAll()
+		.executeTakeFirstOrThrow();
 
 	return sessionDetails;
 }
