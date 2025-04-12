ecbd4aa5a2ce52e32e4170ad39922051ae8e68eb
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-12 14:14:55 +0800

    Delete pg-typed queries

--- apps/api/src/models/queries/requirement/AddRequirement.sql
+++
@@ -1,6 +0,0 @@
-/* @name AddRequirement */
-INSERT INTO hire_me.requirement (role_id, bonus, description)
-	VALUES (:role_id !, :bonus !, :description !)
-RETURNING
-	id, role_id, bonus, description;
-
