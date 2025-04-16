fefff1bf9339a06d16aee6760221ed081a162548
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-16 22:38:04 +1000

    Update company controller to propagate errors

--- apps/api/src/controllers/__tests__/company.controller.test.ts
+++ apps/api/src/controllers/__tests__/company.controller.test.ts
@@ -1,8 +1,4 @@
-import { faker } from "@faker-js/faker";
-import {
-	companyErrorCodes,
-	companyService,
-} from "../../services/company.service";
+import { companyService } from "../../services/company.service";
 import { generateCompany } from "../../testUtils/generators";
 import { getMockReq, getMockRes } from "../../testUtils/index";
 import { handleAddCompany, handleGetCompanies } from "../company.controller";
@@ -17,90 +13,28 @@ beforeEach(() => {
 });
 
 describe("handleAddCompany", () => {
-	describe("when the company does not exist", () => {
-		describe("when the company is successfully added", () => {
-			const company = generateCompany();
-
-			const req = getMockReq({
-				body: company,
-			});
-			const { res, next } = getMockRes();
-
-			beforeEach(() => {
-				mockCreateCompany.mockResolvedValue(company);
-			});
-
-			it("returns a 201 status code", async () => {
-				await handleAddCompany(req, res, next);
-
-				expect(res.status).toHaveBeenCalledWith(201);
-			});
-
-			it("returns the company", async () => {
-				await handleAddCompany(req, res, next);
-
-				expect(res.json).toHaveBeenCalledWith(company);
-			});
-		});
-
-		describe("when there is an error adding the company", () => {
-			const req = getMockReq({
-				body: {
-					name: faker.company.name(),
-				},
-			});
-			const { res, next } = getMockRes();
-			const errorMessage = "Database query failed";
-			const error = new Error(errorMessage);
-
-			beforeEach(() => {
-				mockCreateCompany.mockRejectedValue(error);
-			});
-
-			it("returns a 500 status code", async () => {
-				await handleAddCompany(req, res, next);
-
-				expect(res.status).toHaveBeenCalledWith(500);
-			});
-
-			it("returns an error message", async () => {
-				await handleAddCompany(req, res, next);
-
-				expect(res.json).toHaveBeenCalledWith({
-					error: error.message,
-				});
-			});
-		});
-	});
-
-	describe("when the company already exists", () => {
-		const { res, next } = getMockRes();
-		const companyName = faker.company.name();
+	describe("when the company is successfully added", () => {
+		const company = generateCompany();
 
 		const req = getMockReq({
-			body: {
-				name: companyName,
-			},
+			body: company,
 		});
+		const { res, next } = getMockRes();
 
 		beforeEach(() => {
-			mockCreateCompany.mockRejectedValue(
-				new Error(companyErrorCodes.COMPANY_EXISTS),
-			);
+			mockCreateCompany.mockResolvedValue(company);
 		});
 
-		it("returns a 409 status code", async () => {
+		it("returns a 201 status code", async () => {
 			await handleAddCompany(req, res, next);
 
-			expect(res.status).toHaveBeenCalledWith(409);
+			expect(res.status).toHaveBeenCalledWith(201);
 		});
 
-		it("returns a COMPANY_EXISTS error", async () => {
+		it("returns the company", async () => {
 			await handleAddCompany(req, res, next);
 
-			expect(res.json).toHaveBeenCalledWith({
-				error: companyErrorCodes.COMPANY_EXISTS,
-			});
+			expect(res.json).toHaveBeenCalledWith(company);
 		});
 	});
 });
@@ -126,29 +60,6 @@ describe("handleGetAllCompanies", () => {
 			await handleGetCompanies(req, res, next);
 
 			expect(res.json).toHaveBeenCalledWith(companies);
-		});
-	});
-
-	describe("when there is an error getting the companies", () => {
-		const errorMessage = "Database query failed";
-		const error = new Error(errorMessage);
-
-		beforeEach(() => {
-			mockGetAllCompanies.mockRejectedValue(error);
-		});
-
-		it("returns a 500 status code", async () => {
-			await handleGetCompanies(req, res, next);
-
-			expect(res.status).toHaveBeenCalledWith(500);
-		});
-
-		it("returns an error message", async () => {
-			await handleGetCompanies(req, res, next);
-
-			expect(res.json).toHaveBeenCalledWith({
-				error: error.message,
-			});
 		});
 	});
 });
