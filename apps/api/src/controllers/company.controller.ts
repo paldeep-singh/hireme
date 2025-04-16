eca4669de1839e0a83c472db1dce06ea0cd7acd1
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-16 22:38:04 +1000

    Update company controller to propagate errors

--- apps/api/src/controllers/company.controller.ts
+++ apps/api/src/controllers/company.controller.ts
@@ -1,52 +1,19 @@
 import Company, {
 	CompanyInitializer,
 } from "@repo/api-types/generated/api/hire_me/Company";
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
-import { companyErrorCodes, companyService } from "../services/company.service";
+import { StatusCodes } from "http-status-codes";
+import { companyService } from "../services/company.service";
 import { RequestHandler } from "./sharedTypes";
 
 export const handleAddCompany: RequestHandler<
 	Company,
 	CompanyInitializer
 > = async (req, res) => {
-	try {
-		const company = await companyService.addCompany(req.body);
-		res.status(StatusCodes.CREATED).json(company);
-	} catch (error) {
-		if (error instanceof Error) {
-			if (error.message === companyErrorCodes.COMPANY_EXISTS) {
-				res.status(StatusCodes.CONFLICT).json({
-					error: companyErrorCodes.COMPANY_EXISTS,
-				});
-				return;
-			}
-
-			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-				error: error.message,
-			});
-			return;
-		}
-
-		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-			error: ReasonPhrases.INTERNAL_SERVER_ERROR,
-		});
-	}
+	const company = await companyService.addCompany(req.body);
+	res.status(StatusCodes.CREATED).json(company);
 };
 
 export const handleGetCompanies: RequestHandler<Company[]> = async (_, res) => {
-	try {
-		const companies = await companyService.getCompanies();
-		res.json(companies);
-	} catch (error) {
-		if (error instanceof Error) {
-			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-				error: error.message,
-			});
-			return;
-		}
-
-		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-			error: ReasonPhrases.INTERNAL_SERVER_ERROR,
-		});
-	}
+	const companies = await companyService.getCompanies();
+	res.json(companies);
 };
