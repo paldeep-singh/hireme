fa6507e81118873d71b4ad00029c888655b48417
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-17 12:07:16 +1000

    Update role service and controller to propagate errors to global error handler

--- apps/api/src/controllers/role.controller.ts
+++ apps/api/src/controllers/role.controller.ts
@@ -2,7 +2,7 @@ import Role, {
 	RoleInitializer,
 } from "@repo/api-types/generated/api/hire_me/Role";
 import { RolePreview } from "@repo/api-types/types/api/RolePreview";
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import { roleService } from "../services/role.service";
 import { RequestHandler } from "./sharedTypes";
 
@@ -10,41 +10,15 @@ export const handleAddRole: RequestHandler<Role, RoleInitializer> = async (
 	req,
 	res,
 ) => {
-	try {
-		const role = await roleService.addRole(req.body);
-		res.status(StatusCodes.CREATED).json(role);
-	} catch (error) {
-		if (error instanceof Error) {
-			res
-				.status(StatusCodes.INTERNAL_SERVER_ERROR)
-				.json({ error: error.message });
-			return;
-		}
-
-		res
-			.status(StatusCodes.INTERNAL_SERVER_ERROR)
-			.json({ error: ReasonPhrases.INTERNAL_SERVER_ERROR });
-	}
+	const role = await roleService.addRole(req.body);
+	res.status(StatusCodes.CREATED).json(role);
 };
 
 export const handleGetRolePreviews: RequestHandler<RolePreview[]> = async (
 	_,
 	res,
 ) => {
-	try {
-		const rolePreviews = await roleService.getRolePreviews();
+	const rolePreviews = await roleService.getRolePreviews();
 
-		res.status(StatusCodes.OK).json(rolePreviews);
-	} catch (error) {
-		if (error instanceof Error) {
-			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-				error: error.message,
-			});
-			return;
-		}
-
-		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-			error: ReasonPhrases.INTERNAL_SERVER_ERROR,
-		});
-	}
+	res.status(StatusCodes.OK).json(rolePreviews);
 };
