01633363304ae0f89614abe053d70b6423c37b59
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-24 18:45:57 +1000

    Role details with flat query

--- apps/api/src/models/role.model.ts
+++ apps/api/src/models/role.model.ts
@@ -1,5 +1,5 @@
 import { db } from "../db/database";
-import { NewRole } from "../db/generated/hire_me/Role";
+import { NewRole, RoleId } from "../db/generated/hire_me/Role";
 
 async function addRole(role: NewRole) {
 	return await db
@@ -31,7 +31,60 @@ async function getRolePreviews() {
 		.execute();
 }
 
+async function getRoleDetails(id: RoleId) {
+	const details = await db
+		.withSchema("hire_me")
+		.selectFrom("role")
+		.where("role.id", "=", id)
+		.innerJoin("company", "role.company_id", "company.id")
+		.leftJoin("role_location", "role_location.role_id", "role.id")
+		.leftJoin("application", "role.id", "application.role_id")
+		.leftJoin("contract", "role.id", "contract.role_id")
+		.selectAll("role")
+		.select([
+			"company.name as company_name",
+			"company.notes as company_notes",
+			"company.website as company_website",
+		])
+		.select([
+			"role_location.id as location_id",
+			"role_location.hybrid as location_hybrid",
+			"role_location.location as location_name",
+			"role_location.office_days as location_office_days",
+			"role_location.on_site as location_on_site",
+			"role_location.remote as location_remote",
+		])
+		.select([
+			"application.id as application_id",
+			"application.date_submitted as application_date_submitted",
+			"application.cover_letter as application_cover_letter",
+		])
+		.select([
+			"contract.id as contract_id",
+			"contract.salary_currency as contract_currency",
+			"contract.salary_includes_super as contract_includes_super",
+			"contract.salary_period as contract_salary_period",
+			"contract.salary_range as contract_salary_range",
+			"contract.term as contract_term",
+			"contract.type as contract_type",
+		])
+		.executeTakeFirstOrThrow();
+
+	const requirements = await db
+		.withSchema("hire_me")
+		.selectFrom("requirement")
+		.select(["id", "bonus", "description"])
+		.where("role_id", "=", id)
+		.execute();
+
+	return {
+		...details,
+		requirements,
+	};
+}
+
 export const roleModel = {
 	addRole,
 	getRolePreviews,
+	getRoleDetails,
 };
--- apps/api/src/services/role.service.ts
+++ apps/api/src/services/role.service.ts
@@ -1,6 +1,8 @@
 import Role from "@repo/api-types/generated/api/hire_me/Role";
+import { RoleDetails } from "@repo/api-types/types/api/RoleDetails";
 import { RolePreview } from "@repo/api-types/types/api/RolePreview";
-import { NewRole } from "../db/generated/hire_me/Role";
+import { toNumrangeString } from "@repo/api-types/utils/toNumrangeString";
+import { NewRole, RoleId } from "../db/generated/hire_me/Role";
 import { roleModel } from "../models/role.model";
 
 async function addRole(role: NewRole): Promise<Role> {
@@ -22,7 +24,69 @@ async function getRolePreviews(): Promise<RolePreview[]> {
 	}));
 }
 
+async function getRoleDetails(id: RoleId): Promise<RoleDetails> {
+	const { requirements, ...fetchedDetails } =
+		await roleModel.getRoleDetails(id);
+
+	const role = {
+		id: fetchedDetails.id,
+		ad_url: fetchedDetails.ad_url,
+		date_added: fetchedDetails.date_added.toISOString(),
+		notes: fetchedDetails.notes,
+		title: fetchedDetails.title,
+	};
+
+	const application: RoleDetails["application"] = fetchedDetails.application_id
+		? {
+				id: fetchedDetails.application_id,
+				cover_letter: fetchedDetails.application_cover_letter,
+				date_submitted:
+					fetchedDetails.application_date_submitted?.toISOString() ?? null,
+			}
+		: null;
+
+	const location: RoleDetails["location"] = fetchedDetails.location_id
+		? {
+				id: fetchedDetails.location_id,
+				hybrid: fetchedDetails.location_hybrid!,
+				location: fetchedDetails.location_name!,
+				office_days: toNumrangeString(fetchedDetails.location_office_days!),
+				on_site: fetchedDetails.location_on_site!,
+				remote: fetchedDetails.location_remote!,
+			}
+		: null;
+
+	const company: RoleDetails["company"] = {
+		id: fetchedDetails.company_id,
+		name: fetchedDetails.company_name,
+		notes: fetchedDetails.company_notes,
+		website: fetchedDetails.company_website,
+	};
+
+	const contract: RoleDetails["contract"] = fetchedDetails.contract_id
+		? {
+				id: fetchedDetails.contract_id,
+				salary_currency: fetchedDetails.contract_currency,
+				salary_includes_super: fetchedDetails.contract_includes_super,
+				salary_period: fetchedDetails.contract_salary_period,
+				salary_range: toNumrangeString(fetchedDetails.contract_salary_range),
+				term: fetchedDetails.contract_term?.toISOString() ?? null,
+				type: fetchedDetails.contract_type!,
+			}
+		: null;
+
+	return {
+		...role,
+		application,
+		location,
+		company,
+		contract,
+		requirements,
+	};
+}
+
 export const roleService = {
 	addRole,
 	getRolePreviews,
+	getRoleDetails,
 };
