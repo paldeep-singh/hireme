2cbac6cbb69f044d1e2955405d0c98d48c16d429
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-26 23:13:00 +1000

    Add View Details button to role card

--- apps/jobs-dashboard/src/components/__tests__/RoleCard.test.tsx
+++ apps/jobs-dashboard/src/components/__tests__/RoleCard.test.tsx
@@ -6,7 +6,9 @@ import {
 	generateApiRoleLocationData,
 } from "@repo/api-types/testUtils/generators";
 import { RolePreview } from "@repo/api-types/types/api/RolePreview";
-import { render, screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { renderWithProviders } from "../../testUtils";
 import { RoleCard } from "../RoleCard";
 
 describe("RoleCard", () => {
@@ -24,44 +26,66 @@ describe("RoleCard", () => {
 	};
 
 	it("displays the role title", () => {
-		render(<RoleCard {...rolePreview} />);
+		renderWithProviders(<RoleCard {...rolePreview} />);
 
 		expect(screen.getByText(role.title)).toBeVisible();
 	});
 
 	it("displays the company", () => {
-		render(<RoleCard {...rolePreview} />);
+		renderWithProviders(<RoleCard {...rolePreview} />);
 
 		expect(screen.getByText(company)).toBeVisible();
 	});
 
 	it("displays the notes", () => {
-		render(<RoleCard {...rolePreview} />);
+		renderWithProviders(<RoleCard {...rolePreview} />);
 
 		expect(screen.getByText(role.notes)).toBeVisible();
 	});
 
 	it("displays a link to the ad", () => {
-		render(<RoleCard {...rolePreview} />);
+		renderWithProviders(<RoleCard {...rolePreview} />);
 
-		const link = screen.getByRole("link");
+		const link = screen.queryAllByRole("link")[0];
 
 		expect(link).toHaveTextContent("Ad");
 		expect(link).toHaveAttribute("href", role.ad_url);
 	});
 
+	it("displays a View details button", () => {
+		renderWithProviders(<RoleCard {...rolePreview} />);
+
+		const link = screen.queryAllByRole("link")[1];
+
+		expect(link).toHaveTextContent("View details");
+	});
+
 	it("displays the date the role was added to the dashboard", () => {
-		render(<RoleCard {...rolePreview} />);
+		renderWithProviders(<RoleCard {...rolePreview} />);
 
 		expect(
 			screen.getByText(`Added: ${new Date(role.date_added).toDateString()}`),
 		).toBeVisible();
 	});
 
+	it('navigates to the role details page when "View details" is clicked', async () => {
+		const { router } = renderWithProviders(<RoleCard {...rolePreview} />);
+
+		const user = userEvent.setup();
+
+		await waitFor(async () => {
+			user.click(screen.queryAllByRole("link")[1]);
+
+			expect(router.history.location.pathname).toEqual(
+				`/dashboard/role/${rolePreview.id}`,
+			);
+		});
+	});
+
 	describe("when the role has been submitted", () => {
 		it("displays submitted", () => {
 			const submitted_date = faker.date.recent();
-			render(
+			renderWithProviders(
 				<RoleCard
 					{...rolePreview}
 					date_submitted={submitted_date.toISOString()}
@@ -76,7 +100,7 @@ describe("RoleCard", () => {
 
 	describe("when the role has not been submitted", () => {
 		it("displays Not Submitted", () => {
-			render(<RoleCard {...rolePreview} date_submitted={null} />);
+			renderWithProviders(<RoleCard {...rolePreview} date_submitted={null} />);
 
 			expect(screen.getByText("Not Submitted")).toBeVisible();
 		});
