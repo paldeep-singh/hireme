d0d3c2c11e4ea5b7ee8ae2937e20d6c3f7e60fd3
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-28 16:26:33 +1000

    Add context to add role layout

---
+++ apps/jobs-dashboard/src/routes/dashboard/add-role/route.tsx
@@ -0,0 +1,27 @@
+import { CompanyId } from "@repo/api-types/generated/api/hire_me/Company";
+import { RoleId } from "@repo/api-types/generated/api/hire_me/Role";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { useState } from "react";
+import { AddRoleContext } from "../../../forms/contexts/AddRoleContext";
+
+export const Route = createFileRoute("/dashboard/add-role")({
+	component: AddRoleLayout,
+});
+
+function AddRoleLayout() {
+	const [companyId, setCompanyId] = useState<CompanyId | null>(null);
+	const [roleId, setRoleId] = useState<RoleId | null>(null);
+
+	return (
+		<AddRoleContext.Provider
+			value={{
+				companyId,
+				roleId,
+				setCompanyId,
+				setRoleId,
+			}}
+		>
+			<Outlet />
+		</AddRoleContext.Provider>
+	);
+}
