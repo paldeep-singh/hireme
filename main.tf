20ecf3f68734a9ad5474585259574648ceb7a751
Paldeep Singh <paldeepsingh21@gmail.com>
2025-05-10 13:11:20 +1000

    Add db resource

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   cloud {
     organization = "hire-me"
     workspaces {
-      name = "learn-terraform-aws"
+      name = "hire-me-deployment"
     }
   }
 
@@ -20,11 +20,7 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_instance" "api_server" {
-  ami           = "ami-0f5d1713c9af4fe30"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HireMeApiServer"
-  }
+resource "aws_db_instance" "hire-me-db" {
+  db_name = "hire_me_db"
+  instance_class = "db.t4g.micro"
 }
