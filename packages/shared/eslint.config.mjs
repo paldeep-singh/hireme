72a5144354b86a3dc49a89731739bbe99e121f07
Paldeep Singh <paldeepsingh21@gmail.com>
2025-05-28 21:57:32 +1000

    Extend shared eslint config

--- packages/shared/eslint.config.mjs
+++ packages/shared/eslint.config.mjs
@@ -1,33 +1,3 @@
-import pluginJs from "@eslint/js";
-import * as importPlugin from "eslint-plugin-import";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import packageConfig from "@repo/eslint-config/eslint.config.package.mjs";
 
-export default tseslint.config(
-	{
-		extends: [
-			pluginJs.configs.recommended,
-			...tseslint.configs.recommended,
-			...tseslint.configs.stylisticTypeChecked,
-		],
-		files: ["**/*.ts"],
-		languageOptions: {
-			globals: globals.node,
-			parserOptions: {
-				project: ["tsconfig.json"],
-				tsconfigRootDir: import.meta.dirname,
-			},
-		},
-	},
-	{ rules: { "no-console": "error" }, ignores: ["scripts/**"] },
-	{ files: ["**/*.{js,mjs,cjs,ts}"] },
-	{ languageOptions: { globals: globals.node } },
-	{
-		files: ["**/*.{ts,tsx}"],
-		plugins: { import: importPlugin },
-		rules: {
-			"import/no-duplicates": "error",
-		},
-	},
-	{ ignores: ["dist"] },
-);
+export default packageConfig;
