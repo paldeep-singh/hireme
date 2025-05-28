76d99d1bab03f86839923ea8721b5bc926abf1f8
Paldeep Singh <paldeepsingh21@gmail.com>
2025-05-28 21:51:02 +1000

    Add base eslint config for packages

---
+++ packages/eslint-config/eslint.config.package.mjs
@@ -0,0 +1,33 @@
+import pluginJs from "@eslint/js";
+import * as importPlugin from "eslint-plugin-import";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    extends: [
+      pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ["**/*.ts"],
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        project: ["tsconfig.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  { rules: { "no-console": "error" }, ignores: ["scripts/**"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: { import: importPlugin },
+    rules: {
+      "import/no-duplicates": "error",
+    },
+  },
+  { ignores: ["dist"] }
+);
