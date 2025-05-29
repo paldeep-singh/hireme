a44059ebd8ff0d3dba69130fef05b0ac591600c6
Paldeep Singh <paldeepsingh21@gmail.com>
2025-05-29 18:18:31 +1000

    Just define refinements twice

--- packages/shared/validators/Role.ts
+++ packages/shared/validators/Role.ts
@@ -23,23 +23,24 @@ export const roleInitializerShape: ZodShape<RoleInitializer> = {
 	...roleInputShape,
 };
 
-const withRefinements = <
-	T extends z.ZodObject<{ type: z.ZodTypeAny; term: z.ZodTypeAny }>,
->(
-	schema: T,
-) =>
-	schema
-		.refine((data) => !(data.type === "fixed_term" && data.term === null), {
-			message: "Fixed-term roles must have a term.",
-			path: ["term"],
-		})
-		.refine((data) => !(data.type === "permanent" && data.term !== null), {
-			message: "Permanent roles must not have a term.",
-			path: ["term"],
-		});
+export const roleInputSchema = z
+	.object(roleInputShape)
+	.refine((data) => !(data.type === "fixed_term" && data.term === null), {
+		message: "Fixed-term roles must have a term.",
+		path: ["term"],
+	})
+	.refine((data) => !(data.type === "permanent" && data.term !== null), {
+		message: "Permanent roles must not have a term.",
+		path: ["term"],
+	});
 
-export const roleInputSchema = withRefinements(z.object(roleInputShape));
-
-export const roleInitializerSchema = withRefinements(
-	z.object(roleInitializerShape),
-);
+export const roleInitializerSchema = z
+	.object(roleInitializerShape)
+	.refine((data) => !(data.type === "fixed_term" && data.term === null), {
+		message: "Fixed-term roles must have a term.",
+		path: ["term"],
+	})
+	.refine((data) => !(data.type === "permanent" && data.term !== null), {
+		message: "Permanent roles must not have a term.",
+		path: ["term"],
+	});
