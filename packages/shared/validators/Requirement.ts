febb563754653af0367557ed71f26bba0cad37e5
Paldeep Singh <paldeepsingh21@gmail.com>
2025-05-30 19:27:34 +1000

    Add requirement input schema

--- packages/shared/validators/Requirement.ts
+++ packages/shared/validators/Requirement.ts
@@ -3,18 +3,27 @@ import {
 	RequirementId,
 	RequirementInitializer,
 } from "../generated/api/hire_me/Requirement.js";
+import { OmitStrict } from "../types/utils.js";
 import { ZodShape } from "../utils/zod.js";
 import { roleId } from "./Role.js";
+
+export type RequirementInput = OmitStrict<RequirementInitializer, "role_id">;
 
 export const requirementId = z
 	.number()
 	.transform((val) => val as RequirementId);
 
-export const requirementInitializerShape: ZodShape<RequirementInitializer> = {
-	role_id: roleId,
+export const requirementInputShape: ZodShape<RequirementInput> = {
 	bonus: z.boolean(),
 	description: z.string().min(1),
 };
+
+export const requirementInitializerShape: ZodShape<RequirementInitializer> = {
+	...requirementInputShape,
+	role_id: roleId,
+};
+
+export const requirementInputSchema = z.object(requirementInitializerShape);
 
 export const requirementInitializerSchema = z.object(
 	requirementInitializerShape,
