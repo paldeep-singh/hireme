c61d4c8d23bc377053ef2b9de00fe3f55d463b7e
Paldeep Singh <paldeepsingh21@gmail.com>
2025-05-30 11:46:48 +1000

    Fix linting error

--- apps/jobs-dashboard/src/routes/dashboard/add-role/requirements.tsx
+++ apps/jobs-dashboard/src/routes/dashboard/add-role/requirements.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/dashboard/add-role/requirements')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/dashboard/add-role/requirements")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/add-role/requirements"!</div>
+	return <div>Hello {"/dashboard/add-role/requirements"}</div>;
 }
