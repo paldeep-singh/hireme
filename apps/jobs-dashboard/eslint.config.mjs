cb4034a8d48d8a4913ca302dcb1fa3dffc325540
Paldeep Singh <paldeepsingh21@gmail.com>
2025-05-30 22:29:10 +1000

    Update eslint rule to allow unused underscore variables

--- apps/jobs-dashboard/eslint.config.mjs
+++ apps/jobs-dashboard/eslint.config.mjs
@@ -34,6 +34,18 @@ export default tseslint.config(
 				"warn",
 				{ allowConstantExport: true },
 			],
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{
+					args: "all",
+					argsIgnorePattern: "^_",
+					caughtErrors: "all",
+					caughtErrorsIgnorePattern: "^_",
+					destructuredArrayIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+					ignoreRestSiblings: true,
+				},
+			],
 		},
 	},
 	{
