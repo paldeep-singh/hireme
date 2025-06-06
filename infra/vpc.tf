8b12674b3e554f1bc771d787d4b619d9c7cba106
Paldeep Singh <paldeepsingh21@gmail.com>
2025-06-06 18:01:32 +1000

    Add vpc endpoint

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -201,3 +201,67 @@ resource "aws_db_subnet_group" "rds" {
     Name = "rds-subnet-group"
   }
 }
+
+# VPC Endpoints for SSM
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.ap-southeast-2.ssm"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [aws_subnet.migrations.id]
+
+  security_group_ids = [aws_security_group.vpc_endpoints.id]
+
+  private_dns_enabled = true
+
+  tags = {
+    Name = "ssm-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm_messages" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.ap-southeast-2.ssmmessages"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [aws_subnet.migrations.id]
+
+  security_group_ids = [aws_security_group.vpc_endpoints.id]
+
+  private_dns_enabled = true
+
+  tags = {
+    Name = "ssm-messages-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ec2_messages" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.ap-southeast-2.ec2messages"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [aws_subnet.migrations.id]
+
+  security_group_ids = [aws_security_group.vpc_endpoints.id]
+
+  private_dns_enabled = true
+
+  tags = {
+    Name = "ec2-messages-endpoint"
+  }
+}
+
+# Security group for VPC endpoints
+resource "aws_security_group" "vpc_endpoints" {
+  name        = "vpc-endpoints-sg"
+  description = "Security group for VPC endpoints"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.codebuild.id]
+  }
+
+  tags = {
+    Name = "vpc-endpoints-sg"
+  }
+}
