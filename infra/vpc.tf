7c733a5e7e06892b4b1415984f3b2eaf3a1faeac
Paldeep Singh <paldeepsingh21@gmail.com>
2025-06-06 18:26:28 +1000

    Allow ingress from migrations subnet

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -175,6 +175,14 @@ resource "aws_security_group" "rds" {
     description              = "From EC2"
   }
 
+    ingress {
+    from_port                = 5432
+    to_port                  = 5432
+    protocol                 = "tcp"
+    security_groups          = [aws_security_group.codebuild.id]
+    description              = "From CodeBuild"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
