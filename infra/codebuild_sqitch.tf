874b76fe66af2a430592fbbc1c79d9d9104f1d0c
Paldeep Singh <paldeepsingh21@gmail.com>
2025-06-06 17:03:26 +1000

    Add lifecycle policy for older images

--- infra/codebuild_sqitch.tf
+++ infra/codebuild_sqitch.tf
@@ -95,6 +95,28 @@ resource "aws_ecr_repository" "migration_runner" {
   name = "migration-runner"
 }
 
+resource "aws_ecr_lifecycle_policy" "migration_runner" {
+  repository = aws_ecr_repository.migration_runner.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Delete untagged images older than 1 day"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 1
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_codebuild_project" "sqitch_migrations" {
   name          = "sqitch-migrations"
   description   = "Run Sqitch database migrations"
