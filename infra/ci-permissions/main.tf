efcff61ab0bb461cfade1d012309236a22c0ab2b
Paldeep Singh <paldeepsingh21@gmail.com>
2025-06-17 04:01:39 +0800

    Add policies infra workspace with basic hcp permissions management policies

---
+++ infra/ci-permissions/main.tf
@@ -0,0 +1,31 @@
+variable "AWS_ACCOUNT_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "AWS_REGION" {
+  type      = string
+  sensitive = true
+}
+
+terraform {
+  cloud {
+    organization = "paldeep"
+    workspaces {
+      name = "ci-permissions"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.AWS_REGION
+} 
---
+++ infra/ci-permissions/roles.tf
@@ -0,0 +1,75 @@
+
+resource "aws_iam_role" "ci_permissions_admin" {
+  name = "hire-me-ci-permissions-admin"
+
+  assume_role_policy = jsonencode(({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::${var.AWS_ACCOUNT_ID}:oidc-provider/app.terraform.io"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "app.terraform.io:aud" : "aws.workload.identity"
+          },
+          "StringLike" : {
+            "app.terraform.io:sub" : "organization:paldeep:project:hire-me:workspace:ci-permissions:run_phase:*"
+          }
+        }
+      }
+    ]
+  }))
+}
+
+resource "aws_iam_role" "deployment_admin" {
+  name = "hire-me-deployment-admin"
+
+  assume_role_policy = jsonencode(({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::${var.AWS_ACCOUNT_ID}:oidc-provider/app.terraform.io"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "app.terraform.io:aud" : "aws.workload.identity"
+          },
+          "StringLike" : {
+            "app.terraform.io:sub" : "organization:paldeep:project:hire-me:workspace:deployment:run_phase:*"
+          }
+        }
+      }
+    ]
+  }))
+}
+
+resource "aws_iam_role" "db_migrations_admin" {
+  name = "hire-me-db-migrations-admin"
+
+  assume_role_policy = jsonencode(({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::${var.AWS_ACCOUNT_ID}:oidc-provider/app.terraform.io"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "app.terraform.io:aud" : "aws.workload.identity"
+          },
+          "StringLike" : {
+            "app.terraform.io:sub" : "organization:paldeep:project:hire-me:workspace:db-migrations:run_phase:*"
+          }
+        }
+      }
+    ]
+  }))
+}
