86deddef71a8cc7fca70e1c74abf8c48ef1a19f6
Paldeep Singh <paldeepsingh21@gmail.com>
2025-06-30 17:20:35 +1000

    Regen route types

--- packages/api-fetch/generated/routes/salary.ts
+++ packages/api-fetch/generated/routes/salary.ts
@@ -1,12 +1,11 @@
-import Salary, {
-	SalaryInitializer,
-} from "@repo/api-types/generated/api/hire_me/Salary";
+import Salary from "@repo/api-types/generated/api/hire_me/Salary";
+import { SalaryInput } from "@repo/api-types/validators/Salary";
 
 // This file is generated and should not be modified directly.
 export interface AddSalaryRequest {
 	method: "post";
-	path: "/api/salary";
-	params: null;
+	path: "/api/role/:role_id/salary";
+	params: { role_id: number };
 	responseBody: Salary;
-	body: SalaryInitializer;
+	body: SalaryInput;
 }
