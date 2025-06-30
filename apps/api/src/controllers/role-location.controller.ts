b81162ea47d4ad0de49e5cc39938d924cbc6e926
Paldeep Singh <paldeepsingh21@gmail.com>
2025-06-30 16:59:31 +1000

    Refactor location route

--- apps/api/src/controllers/role-location.controller.ts
+++ apps/api/src/controllers/role-location.controller.ts
@@ -1,15 +1,19 @@
-import RoleLocation, {
-	RoleLocationInitializer,
-} from "@repo/api-types/generated/api/hire_me/RoleLocation";
+import RoleLocation from "@repo/api-types/generated/api/hire_me/RoleLocation";
+import { RoleLocationInput } from "@repo/api-types/validators/RoleLocation";
 import { StatusCodes } from "http-status-codes";
+import { RoleId } from "../db/generated/hire_me/Role";
 import { roleLocationService } from "../services/role-location.service";
 import { RequestHandler } from "./sharedTypes";
 
 export const handleAddRoleLocation: RequestHandler<
 	RoleLocation,
-	RoleLocationInitializer
+	RoleLocationInput,
+	{ role_id: number }
 > = async (req, res) => {
-	const roleLocation = await roleLocationService.addRoleLocation(req.body);
+	const roleLocation = await roleLocationService.addRoleLocation({
+		...req.body,
+		role_id: req.parsedParams.role_id as RoleId,
+	});
 
 	res.status(StatusCodes.CREATED).json(roleLocation);
 };
--- apps/api/src/routes/__tests__/role-location.route.integration.test.ts
+++ apps/api/src/routes/__tests__/role-location.route.integration.test.ts
@@ -1,4 +1,5 @@
 import { generateApiRoleLocationData } from "@repo/api-types/testUtils/generators";
+import { omit } from "lodash-es";
 import request from "supertest";
 import api from "../../api";
 import { db } from "../../db/database";
@@ -19,7 +20,7 @@ afterAll(async () => {
 	await db.withSchema("hire_me").destroy(); // Close the pool after each test file
 });
 
-describe("POST /api/role-location", () => {
+describe("POST /api/role_id/location", () => {
 	let role: Role;
 
 	beforeEach(async () => {
@@ -43,24 +44,26 @@ describe("POST /api/role-location", () => {
 		describe("when valid body is provided", () => {
 			it("returns status code 201", async () => {
 				const roleLocationData = generateApiRoleLocationData(role.id);
+				const locationInput = omit(roleLocationData, ["role_id"]);
 
 				const response = await request(api)
-					.post("/api/role-location")
+					.post(`/api/role/${role.id}/location`)
 					.set("Cookie", [`session=${JSON.stringify({ id: session.id })}`])
-					.send(roleLocationData);
+					.send(locationInput);
 
 				expect(response.status).toEqual(201);
 			});
 
 			it("returns the role location", async () => {
 				const roleLocationData = generateApiRoleLocationData(role.id);
+				const locationInput = omit(roleLocationData, ["role_id"]);
 
 				const {
 					body: { id, ...rest },
 				} = await request(api)
-					.post("/api/role-location")
+					.post(`/api/role/${role.id}/location`)
 					.set("Cookie", [`session=${JSON.stringify({ id: session.id })}`])
-					.send(roleLocationData);
+					.send(locationInput);
 
 				expect(id).toBeNumber();
 				expect(rest).toEqual(roleLocationData);
@@ -70,7 +73,7 @@ describe("POST /api/role-location", () => {
 		describe("when invalid body is provided", () => {
 			it("returns statusCode 400", async () => {
 				const response = await request(api)
-					.post("/api/role-location")
+					.post(`/api/role/${role.id}/location`)
 					.set("Cookie", [`session=${JSON.stringify({ id: session.id })}`])
 					.send({});
 				expect(response.status).toBe(400);
@@ -78,7 +81,7 @@ describe("POST /api/role-location", () => {
 
 			it("returns an error message", async () => {
 				const response = await request(api)
-					.post("/api/role-location")
+					.post(`/api/role/${role.id}/location`)
 					.set("Cookie", [`session=${JSON.stringify({ id: session.id })}`])
 					.send({});
 
@@ -90,20 +93,24 @@ describe("POST /api/role-location", () => {
 	describe("when no session is provided", () => {
 		it("returns statusCode 400", async () => {
 			const roleLocationData = generateRoleLocationData(role.id);
+			const locationInput = omit(roleLocationData, ["role_id"]);
 
 			const response = await request(api)
-				.post("/api/role-location")
-				.send(roleLocationData);
+				.post(`/api/role/${role.id}/location`)
+				.send(locationInput);
 
 			expect(response.status).toBe(400);
 		});
 
 		it("returns the a BAD_REQUEST error message", async () => {
 			const roleLocationData = generateRoleLocationData(role.id);
+			const locationInput = omit(roleLocationData, ["role_id"]);
 
 			const {
 				body: { error },
-			} = await request(api).post("/api/role-location").send(roleLocationData);
+			} = await request(api)
+				.post(`/api/role/${role.id}/location`)
+				.send(locationInput);
 
 			expect(error).toEqual(authorisationErrorMessages.BAD_REQUEST);
 		});
